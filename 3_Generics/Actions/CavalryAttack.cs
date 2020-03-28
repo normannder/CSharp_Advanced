b449694e4a44fc3cc33bdb7d6975049a8f96e632
normannder <deman.channel@gmail.com>
2020-03-28 15:09:06 +0200

    game update

---
+++ 3_Generics/Actions/CavalryAttack.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StrategyGame.Warriors.Models;
+using StrategyGame.Warriors.Interfaces;
+using StrategyGame.Warriors.Abstractions;
+using static ITEA_Collections.Common.Extensions;
+
+namespace StrategyGame.Actions
+{
+    class CavalryAttack<T> : Battle<Knight, T> where T:  CombatUnit, IRangeUnit, new()
+    {
+        protected override int CountPoints(IEnumerable<CombatUnit> army)
+        {
+            int total = 0;
+            int count = 0;
+            foreach (var item in army)
+            {
+                count++;
+                total += item.Health + (int)(item.Strength * (item.UnitType == CombatUnitType.Melee ? 2 : 1));
+            }
+            ToConsole($"Total army count: {count}", ConsoleColor.DarkYellow);
+            ToConsole($"Total army strength: {total}", ConsoleColor.DarkYellow);
+            return total;
+        }
+    }
+}
---
+++ 3_Generics/Warriors/Models/Knight.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StrategyGame.Base.Interfaces;
+using StrategyGame.Warriors.Abstractions;
+using StrategyGame.Warriors.Interfaces;
+
+namespace StrategyGame.Warriors.Models
+{
+    public sealed class Knight : CombatUnit
+    {
+        public Knight() : base(20, 15, CombatUnitType.Melee)
+        {
+        }
+
+        public override void MeleeAttack(IUnit enemy)
+        {
+            enemy.Health -= this.Strength;
+        }
+
+
+        public override string Unnamed() => "For the King!";
+
+    }
+}
