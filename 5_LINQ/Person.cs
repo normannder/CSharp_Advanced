cf10ae39c96a3230c9a7874de49f08ab54311c00
Alexander <alexandersviridenko98@gmail.com>
2020-04-03 19:57:47 +0300

    extebd Address

--- 5_LINQ/Person.cs
+++ 5_LINQ/Person.cs
@@ -19,6 +19,8 @@ namespace IteaLinq
         public string PasswordHash { get; set; }
         public string PasswordSalt { get; set; }
         public string City { get; set; }
+        public string Street { get; set; }
+        public string Appartment { get; set; }
 
         public Person(string name, int age)
         {
