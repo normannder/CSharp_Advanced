12ad6ef92eb508a5b03b08b6334f7eff841df92e
normannder <deman.channel@gmail.com>
2020-04-24 14:53:43 +0300

    update

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Lesson_1
 {
@@ -6,22 +8,43 @@ namespace Lesson_1
     {
         static void Main(string[] args)
         {
+            List<string> characters = new List<string>();
+            List<int> numbers = new List<int>();
 
-            var anon = new
+            string pathtofile  = @"C:\Users\Asus PC\Desktop\1.txt";
+            string pathtofile2 = @"C:\Users\Asus PC\Desktop\2.txt";
+            string pathtofile3 = @"C:\Users\Asus PC\Desktop\3.txt";
+            using (StreamReader stream = new StreamReader(pathtofile))
             {
-                Name = "Anon",
-                Age = 21
-            };
+                int number;
+                bool result;
+                string[] str = stream.ReadToEnd().Split(' ');
+                foreach (var item in str)
+                {
+                    result = int.TryParse(item, out number);
+                    if (result)
+                        numbers.Add(number);
+                    else
+                        characters.Add(item.ToString());
+                }
+                using (StreamWriter stream2 = new StreamWriter(pathtofile2))
+                {
+                    foreach(string line in characters)
+                    {
+                        stream2.WriteLine(line);
+                    }
+                }
+                using (StreamWriter stream3 = new StreamWriter(pathtofile3))
+                {
+                    foreach (int item in numbers)
+                    {
+                        stream3.WriteLine(item);
+                    }
+                }
+            }
+            
 
-            var anon1 = new
-            {
-                Name = "Anon",
-                Age = "dwqd"
-            };
-
-            Console.WriteLine(anon.GetType().Name);
-            Console.WriteLine(anon1.GetType().Name);
-            Console.WriteLine("Hello World!");
         }
     }
 }
+            
